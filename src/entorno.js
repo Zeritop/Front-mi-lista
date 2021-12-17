0d2907e42bbd972ce62f98c3bd05d6da61cb8d7f
Zeritop <80865749+Zeritop@users.noreply.github.com>
2021-12-17 20:40:08 -0300

    Update entorno.js

--- src/entorno.js
+++ src/entorno.js
@@ -1,4 +1,4 @@
-const API = window.location.hostname === 'localhost' || window.location.hostname === "127.0.0.1" ? 'http://localhost:4000' : 'https://backend-mi-lista.herokuapp.com';
+const API = window.location.hostname === 'localhost' || window.location.hostname === "127.0.0.1" ? 'http://localhost:4000' : 'https://backend-mi-lista.herokuapp.com/';
 
 export {
     API
